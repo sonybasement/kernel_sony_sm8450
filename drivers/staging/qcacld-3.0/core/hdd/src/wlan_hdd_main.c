579276eb724d278ad635370255e468f238d6075d
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-07 11:11:33 +0900

    qcacld-3.0: Nuke rx_wakelock code entirely
    qcom_rx_wakelock is one of the major culprit to increased power consumption.
    
    Although its total wakelock time is quite low, it's catched very frequently
    and prevents the system from entering suspend repeatedly.
    
    Original intention of this wakelock is to prevent dropping unicast or
    local ARP packet, but I'm having a hard time understanding why are those
    packets critical to regular Android users.
    
    qcacld's packet filter will still allow notifications to flow in.
    
    Stop allowing this to be configurable on this kernel.
    Nuke this entirely to reduce resource usage.
    
    Change-Id: I8ae655b871e58dc1c9a880c831cc6a5e2d118594

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -9727,32 +9727,6 @@ out:
 }
 
 /**
- * hdd_rx_wake_lock_destroy() - Destroy RX wakelock
- * @hdd_ctx:	HDD context.
- *
- * Destroy RX wakelock.
- *
- * Return: None.
- */
-static void hdd_rx_wake_lock_destroy(struct hdd_context *hdd_ctx)
-{
-	qdf_wake_lock_destroy(&hdd_ctx->rx_wake_lock);
-}
-
-/**
- * hdd_rx_wake_lock_create() - Create RX wakelock
- * @hdd_ctx:	HDD context.
- *
- * Create RX wakelock.
- *
- * Return: None.
- */
-static void hdd_rx_wake_lock_create(struct hdd_context *hdd_ctx)
-{
-	qdf_wake_lock_create(&hdd_ctx->rx_wake_lock, "qcom_rx_wakelock");
-}
-
-/**
  * hdd_context_deinit() - Deinitialize HDD context
  * @hdd_ctx:    HDD context.
  *
@@ -9771,8 +9745,6 @@ static int hdd_context_deinit(struct hdd_context *hdd_ctx)
 	hdd_bbm_context_deinit(hdd_ctx);
 
 	hdd_sap_context_destroy(hdd_ctx);
-
-	hdd_rx_wake_lock_destroy(hdd_ctx);
 
 	hdd_scan_context_destroy(hdd_ctx);
 
@@ -13001,8 +12973,6 @@ static int hdd_context_init(struct hdd_context *hdd_ctx)
 	if (ret)
 		goto list_destroy;
 
-	hdd_rx_wake_lock_create(hdd_ctx);
-
 	ret = hdd_sap_context_init(hdd_ctx);
 	if (ret)
 		goto scan_destroy;
@@ -13033,7 +13003,6 @@ sap_destroy:
 
 scan_destroy:
 	hdd_scan_context_destroy(hdd_ctx);
-	hdd_rx_wake_lock_destroy(hdd_ctx);
 list_destroy:
 	qdf_list_destroy(&hdd_ctx->hdd_adapters);
 
